61bb2a06b9e98c4edd10bda69e079e5a36901f8d
Dominika Bochenczyk <dominika.bochenczyk@gmail.com>
2024-11-23 18:35:09 +0100

    show sphere properly

--- apps/main.cpp
+++ apps/main.cpp
@@ -13,20 +13,20 @@ auto main() -> int {
 
   std::vector<uchar4> image(static_cast<size_t>(image_width * image_height));
 
-  const Sphere sphere = {Vec3(0.0, 0.0, -1.0), 0.5};
+  const Sphere sphere = {Vec3(40.0, 40.0, 0.0), 10};
 
-  // make a cool gradient :)
+  // make a cool gradient with a sphere :)
   for (int i = 0; i < image_height; i++) {
     for (int j = 0; j < image_width; j++) {
-      float const r = static_cast<float>(i) / (image_width - 1);
-      float const g = static_cast<float>(j) / (image_height - 1);
-      constexpr float b = 0.25;
+      float r = static_cast<float>(i) / (image_width - 1);
+      float g = static_cast<float>(j) / (image_height - 1);
+      float b = 0.25;
 
-      // if (sphere.hit(Ray(Vec3(0.0, 0.0, 0.0), Vec3(0.0, 0.0, -1.0))) > 0.0) {
-      //   r = 1.0;
-      //   g = 0.0;
-      //   b = 0.0;
-      // }
+      if (sphere.hit(Ray(Vec3(i, j, -1), Vec3(0, 0, 1)))) {
+        r = 0;
+        g = 0;
+        b = 1;
+      }
 
       image[i * image_width + j] =
           convertColorTo8Bit(make_float4(r, g, b, 1.0));
