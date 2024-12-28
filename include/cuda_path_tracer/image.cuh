4c1b4b21f211615d4babaa8095b172a179250e77
eduard <occhipinti.eduard@icloud.com>
2024-12-28 19:41:13 +0100

    feat: use thrust vectors instead of manually allocating raw pointers

--- include/cuda_path_tracer/image.cuh
+++ include/cuda_path_tracer/image.cuh
@@ -12,8 +12,10 @@
 
 #pragma once
 
+#include <cstdint>
 #include <driver_types.h>
-#include <vector>
+#include <string>
+#include <thrust/host_vector.h>
 #include <vector_types.h>
 
 /**
@@ -24,8 +26,9 @@
  * @param height Height of the image
  * @param image Image to save
  */
-__host__ void saveImageAsPPM(const char *filename, int width, int height,
-                             const std::vector<uchar4> &image);
+__host__ void saveImageAsPPM(const std::string &filename, const uint16_t width,
+                             const uint16_t height,
+                             const thrust::host_vector<uchar4> &image);
 
 /**
  * @brief Convert a float4 color to a char4 color
