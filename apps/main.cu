07ee30de03eefae9bb99922c7cf0a6e0c24caa97
eduard <occhipinti.eduard@icloud.com>
2025-02-09 11:44:58 +0100

    feat: add some color utilities and default colors

--- apps/main.cu
+++ apps/main.cu
@@ -1,8 +1,7 @@
-
 #include "cuda_path_tracer/camera.cuh"
+#include "cuda_path_tracer/color.cuh"
 #include "cuda_path_tracer/image.cuh"
 #include "cuda_path_tracer/lambertian.cuh"
-#include "cuda_path_tracer/metal.cuh"
 #include "cuda_path_tracer/shapes/sphere.cuh"
 #include <cstdlib>
 #include <thrust/device_vector.h>
@@ -10,11 +9,14 @@
 #include <vector_functions.h>
 #include <vector_types.h>
 
+namespace {
 void demo3Spheres(const uint16_t image_width, const uint16_t image_height,
                   thrust::universal_host_pinned_vector<uchar4> &image) {
   const auto shapes = thrust::device_vector<Shape>{
-      Sphere{{0, 0, -1.2}, 0.5, Lambertian(Vec3{0.1, 0.2, 0.5})}, Sphere{{-1, 0, -1}, 0.5, Dielectric(1.50)},
-      Sphere{{1, 0, -1}, 0.5, Dielectric(1.00 / 1.50)}, Sphere{{0, -100.5, -1}, 100, Lambertian(Vec3{0.8, 0.8, 0.0})}};
+      Sphere{{0, 0, -1.2}, 0.5, Lambertian(Catpuccin::Latte::Flamingo)},
+      Sphere{{-1, 0, -1}, 0.5, Dielectric(1.50)},
+      Sphere{{1, 0, -1}, 0.5, Dielectric(1.00 / 1.50)},
+      Sphere{{0, -100.5, -1}, 100, Lambertian(Vec3{0.8, 0.8, 0.0})}};
   const auto scene = std::make_shared<Scene>(image_width, image_height, shapes);
 
   auto camera = CameraBuilder()
@@ -56,6 +58,7 @@ void cornellBox(const uint16_t image_width, const uint16_t image_height,
                     .build();
   camera.render(scene, image);
 }
+} // namespace
 
 auto main() -> int {
   constexpr auto image_width = 512;
--- include/cuda_path_tracer/vec3.cuh
+++ include/cuda_path_tracer/vec3.cuh
@@ -11,8 +11,8 @@
 
 #pragma once
 
-#include <ostream>
 #include <curand_kernel.h>
+#include <ostream>
 
 /**
  * @brief Class for a 3D vector
@@ -21,10 +21,14 @@
 struct Vec3 {
   float x, y, z;
 
-  __host__ __device__ Vec3();
-  __host__ __device__ Vec3(const float x);
-  __host__ __device__ Vec3(const float x, const float y, const float z);
-  __host__ __device__ Vec3(const float4 &v);
+  __host__ __device__ constexpr Vec3() : x(0), y(0), z(0) {}
+  __host__ __device__ constexpr Vec3(const float value)
+      : x(value), y(value), z(value) {}
+  __host__ __device__ constexpr Vec3(const float x_, const float y_,
+                                     const float z_)
+      : x(x_), y(y_), z(z_) {}
+  __host__ __device__ constexpr Vec3(const float4 &v)
+      : x(v.x), y(v.y), z(v.z) {}
 
   __host__ __device__ auto operator-() const -> Vec3;
   __host__ __device__ auto operator==(const Vec3 &v) const -> bool;
@@ -55,8 +59,11 @@ __host__ __device__ auto cross(const Vec3 &v1, const Vec3 &v2) -> Vec3;
 __host__ __device__ auto dot(const Vec3 &v1, const Vec3 &v2) -> float;
 
 __host__ __device__ auto makeUnitVector(const Vec3 &v) -> Vec3;
-__device__ auto vectorOnHemisphere(const Vec3 &v, curandStatePhilox4_32_10_t &state) -> Vec3;
+__device__ auto vectorOnHemisphere(const Vec3 &v,
+                                   curandStatePhilox4_32_10_t &state) -> Vec3;
 __device__ auto randomVector(curandStatePhilox4_32_10_t &state) -> Vec3;
-__device__ auto roundScatterDirection(const Vec3 &direction, const Vec3 &normal) -> Vec3;
+__device__ auto roundScatterDirection(const Vec3 &direction,
+                                      const Vec3 &normal) -> Vec3;
 __device__ auto reflect(const Vec3 &v, const Vec3 &n) -> Vec3;
-__device__ auto refract(const Vec3 &v, const Vec3 &n, double eta_component) -> Vec3;
+__device__ auto refract(const Vec3 &v, const Vec3 &n,
+                        double eta_component) -> Vec3;
