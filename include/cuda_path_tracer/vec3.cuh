ed25812136539525c4685a43926715a182290932
eduard <occhipinti.eduard@icloud.com>
2025-02-12 20:27:16 +0100

    feat: template for different vector on hemisphere implementations

--- include/cuda_path_tracer/vec3.cuh
+++ include/cuda_path_tracer/vec3.cuh
@@ -48,9 +48,13 @@ __host__ __device__ auto cross(const Vec3 &v1, const Vec3 &v2) -> Vec3;
 __host__ __device__ auto dot(const Vec3 &v1, const Vec3 &v2) -> float;
 
 __host__ __device__ auto makeUnitVector(const Vec3 &v) -> Vec3;
-__device__ auto vectorOnHemisphere(const Vec3 &v,
-                                   curandStatePhilox4_32_10_t &state) -> Vec3;
+
+template <typename State>
+__device__ auto vectorOnHemisphere(const Vec3 &v, State &state) -> Vec3;
+
 __device__ auto randomVector(curandStatePhilox4_32_10_t &state) -> Vec3;
+__device__ auto randomVector(curandState_t &state) -> Vec3;
+
 __device__ auto roundScatterDirection(const Vec3 &direction,
                                       const Vec3 &normal) -> Vec3;
 __device__ auto reflect(const Vec3 &v, const Vec3 &n) -> Vec3;
