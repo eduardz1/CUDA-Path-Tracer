a04aac59b36594c752e38da33e44b8b0511a9c6e
eduard <occhipinti.eduard@icloud.com>
2025-02-12 20:26:06 +0100

    chore: extracted different getRay implementations

--- include/cuda_path_tracer/ray.cuh
+++ include/cuda_path_tracer/ray.cuh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "cuda_path_tracer/vec3.cuh"
+#include <cuda/std/tuple>
 
 class Ray {
 public:
@@ -21,3 +22,25 @@ public:
 private:
   Vec3 origin, direction;
 };
+
+// Functions to generate rays
+
+__device__ auto getRay(const Vec3 &origin, const Vec3 &pixel00,
+                       const Vec3 &deltaU, const Vec3 &deltaV,
+                       const Vec3 &defocusDiskU, const Vec3 &defocusDiskV,
+                       const float defocusAngle, const uint16_t x,
+                       const uint16_t y, curandState_t &state) -> Ray;
+
+__device__ auto get2Rays(const Vec3 &origin, const Vec3 &pixel00,
+                         const Vec3 &deltaU, const Vec3 &deltaV,
+                         const Vec3 &defocusDiskU, const Vec3 &defocusDiskV,
+                         const float defocusAngle, const uint16_t x,
+                         const uint16_t y, curandStatePhilox4_32_10_t &state)
+    -> cuda::std::tuple<Ray, Ray>;
+
+__device__ auto get4Rays(const Vec3 &origin, const Vec3 &pixel00,
+                         const Vec3 &deltaU, const Vec3 &deltaV,
+                         const Vec3 &defocusDiskU, const Vec3 &defocusDiskV,
+                         const float defocusAngle, const uint16_t x,
+                         const uint16_t y, curandStatePhilox4_32_10_t &state)
+    -> cuda::std::tuple<Ray, Ray, Ray, Ray>;
