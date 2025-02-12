911c527f33d7cda8fd0a0e058f4c7befeaec5894
eduard <occhipinti.eduard@icloud.com>
2025-02-12 20:23:06 +0100

    test: ray and benchmark different implementations

--- src/ray.cu
+++ src/ray.cu
@@ -1,3 +1,4 @@
+#include "cuda_path_tracer/camera.cuh"
 #include "cuda_path_tracer/ray.cuh"
 #include "cuda_path_tracer/vec3.cuh"
 
@@ -10,4 +11,114 @@ __host__ __device__ auto Ray::getDirection() const -> Vec3 { return direction; }
 
 __host__ __device__ auto Ray::at(const float t) const -> Vec3 {
   return origin + direction * t;
+}
+
+__device__ auto getRay(const Vec3 &origin, const Vec3 &pixel00,
+                       const Vec3 &deltaU, const Vec3 &deltaV,
+                       const Vec3 &defocusDiskU, const Vec3 &defocusDiskV,
+                       const float defocusAngle, const uint16_t x,
+                       const uint16_t y, curandState_t &state) -> Ray {
+  const auto a = curand_uniform(&state);
+  const auto b = curand_uniform(&state);
+
+  // We sample an area of "half pixel" around the pixel centers to achieve
+  // anti-aliasing. This helps in reducing the jagged edges by averaging the
+  // colors of multiple samples within each pixel, resulting in smoother
+  // transitions and more realistic images.
+  const auto offset = Vec3{a - 0.5F, b - 0.5F, 0};
+
+  const auto sample = pixel00 + ((static_cast<float>(x) + offset.x) * deltaU) +
+                      ((static_cast<float>(y) + offset.y) * deltaV);
+
+  auto newOrigin = origin;
+
+  if (defocusAngle > 0) {
+    newOrigin = defocusDiskSample(state, origin, defocusDiskU, defocusDiskV);
+  }
+
+  const auto direction = sample - newOrigin;
+
+  return {newOrigin, direction};
+}
+
+__device__ auto get2Rays(const Vec3 &origin, const Vec3 &pixel00,
+                         const Vec3 &deltaU, const Vec3 &deltaV,
+                         const Vec3 &defocusDiskU, const Vec3 &defocusDiskV,
+                         const float defocusAngle, const uint16_t x,
+                         const uint16_t y, curandStatePhilox4_32_10_t &state)
+    -> cuda::std::tuple<Ray, Ray> {
+  const auto values = curand_uniform4(&state);
+
+  const auto offsetA = Vec3{values.z - 0.5F, values.w - 0.5F, 0};
+  const auto offsetB = Vec3{values.x - 0.5F, values.y - 0.5F, 0};
+
+  const auto sampleA = pixel00 +
+                       ((static_cast<float>(x) + offsetA.x) * deltaU) +
+                       ((static_cast<float>(y) + offsetA.y) * deltaV);
+  const auto sampleB = pixel00 +
+                       ((static_cast<float>(x) + offsetB.x) * deltaU) +
+                       ((static_cast<float>(y) + offsetB.y) * deltaV);
+
+  auto newOriginA = origin;
+  auto newOriginB = origin;
+
+  if (defocusAngle > 0) {
+    newOriginA = defocusDiskSample(state, origin, defocusDiskU, defocusDiskV);
+    newOriginB = defocusDiskSample(state, origin, defocusDiskU, defocusDiskV);
+  }
+
+  const auto directionA = sampleA - newOriginA;
+  const auto directionB = sampleB - newOriginB;
+
+  return {{newOriginA, directionA}, {newOriginB, directionB}};
+}
+
+__device__ auto get4Rays(const Vec3 &origin, const Vec3 &pixel00,
+                         const Vec3 &deltaU, const Vec3 &deltaV,
+                         const Vec3 &defocusDiskU, const Vec3 &defocusDiskV,
+                         const float defocusAngle, const uint16_t x,
+                         const uint16_t y, curandStatePhilox4_32_10_t &state)
+    -> cuda::std::tuple<Ray, Ray, Ray, Ray> {
+  const auto values1 = curand_uniform4(&state);
+  const auto values2 = curand_uniform4(&state);
+
+  const auto offsetA = Vec3{values1.z - 0.5F, values1.w - 0.5F, 0};
+  const auto offsetB = Vec3{values1.x - 0.5F, values1.y - 0.5F, 0};
+  const auto offsetC = Vec3{values2.z - 0.5F, values2.w - 0.5F, 0};
+  const auto offsetD = Vec3{values2.x - 0.5F, values2.y - 0.5F, 0};
+
+  const auto sampleA = pixel00 +
+                       ((static_cast<float>(x) + offsetA.x) * deltaU) +
+                       ((static_cast<float>(y) + offsetA.y) * deltaV);
+  const auto sampleB = pixel00 +
+                       ((static_cast<float>(x) + offsetB.x) * deltaU) +
+                       ((static_cast<float>(y) + offsetB.y) * deltaV);
+  const auto sampleC = pixel00 +
+                       ((static_cast<float>(x) + offsetC.x) * deltaU) +
+                       ((static_cast<float>(y) + offsetC.y) * deltaV);
+  const auto sampleD = pixel00 +
+                       ((static_cast<float>(x) + offsetD.x) * deltaU) +
+                       ((static_cast<float>(y) + offsetD.y) * deltaV);
+
+  auto newOriginA = origin;
+  auto newOriginB = origin;
+  auto newOriginC = origin;
+  auto newOriginD = origin;
+
+  if (defocusAngle > 0) {
+    const auto [s1, s2, s3, s4] =
+        defocusDisk4Samples(state, origin, defocusDiskU, defocusDiskV);
+    newOriginA = s1;
+    newOriginB = s2;
+    newOriginC = s3;
+    newOriginD = s4;
+  }
+
+  const auto directionA = sampleA - newOriginA;
+  const auto directionB = sampleB - newOriginB;
+
+  return {{newOriginA, directionA},
+          {newOriginB, directionB},
+          {newOriginC, sampleC - newOriginC},
+          {newOriginD, sampleD - newOriginD}};
 }
--- tests/test_ray.cu
+++ tests/test_ray.cu
@@ -1,8 +1,104 @@
+#include "cuda_path_tracer/error.cuh"
 #include "cuda_path_tracer/ray.cuh"
 #include "cuda_path_tracer/vec3.cuh"
+#include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
-// NOLINTBEGIN(cppcoreguidelines-avoid-do-while,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+// NOLINTBEGIN(cppcoreguidelines-pro-bounds-pointer-arithmetic,cppcoreguidelines-avoid-do-while,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+
+namespace {
+constexpr auto num_samples = 1024;
+constexpr auto seed = 808;
+
+__global__ void benchmarkGetRay(Ray *results, const Vec3 origin,
+                                const Vec3 pixel00, const Vec3 deltaU,
+                                const Vec3 deltaV, const Vec3 defocusDiskU,
+                                const Vec3 defocusDiskV,
+                                const float defocusAngle,
+                                const int num_samples) {
+  curandState_t state;
+  curand_init(seed, 0, 0, &state);
+
+  for (int i = 0; i < num_samples; i++) {
+    results[i] = getRay(origin, pixel00, deltaU, deltaV, defocusDiskU,
+                        defocusDiskV, defocusAngle, 0, 0, state);
+  }
+}
+
+__global__ void benchmarkGet2Rays(Ray *results, const Vec3 origin,
+                                  const Vec3 pixel00, const Vec3 deltaU,
+                                  const Vec3 deltaV, const Vec3 defocusDiskU,
+                                  const Vec3 defocusDiskV,
+                                  const float defocusAngle,
+                                  const int num_samples) {
+  curandStatePhilox4_32_10_t state;
+  curand_init(seed, 0, 0, &state);
+
+  for (int i = 0; i < num_samples; i += 2) {
+    auto [r1, r2] = get2Rays(origin, pixel00, deltaU, deltaV, defocusDiskU,
+                             defocusDiskV, defocusAngle, 0, 0, state);
+    results[i] = r1;
+    results[i + 1] = r2;
+  }
+}
+
+__global__ void benchmarkGet4Rays(Ray *results, const Vec3 origin,
+                                  const Vec3 pixel00, const Vec3 deltaU,
+                                  const Vec3 deltaV, const Vec3 defocusDiskU,
+                                  const Vec3 defocusDiskV,
+                                  const float defocusAngle,
+                                  const int num_samples) {
+  curandStatePhilox4_32_10_t state;
+  curand_init(seed, 0, 0, &state);
+
+  for (int i = 0; i < num_samples; i += 4) {
+    auto [r1, r2, r3, r4] =
+        get4Rays(origin, pixel00, deltaU, deltaV, defocusDiskU, defocusDiskV,
+                 defocusAngle, 0, 0, state);
+    results[i] = r1;
+    results[i + 1] = r2;
+    results[i + 2] = r3;
+    results[i + 3] = r4;
+  }
+}
+} // namespace
+
+TEST_CASE("Ray Generation Benchmarks", "[benchmark]") {
+  Ray *d_results = nullptr;
+  CUDA_ERROR_CHECK(cudaMalloc(&d_results, num_samples * sizeof(Ray)));
+
+  // Setup test parameters
+  const Vec3 origin(0.0F, 0.0F, 0.0F);
+  const Vec3 pixel00(-1.0F, 1.0F, -1.0F);
+  const Vec3 deltaU(2.0F / num_samples, 0.0F, 0.0F);
+  const Vec3 deltaV(0.0F, -2.0F / num_samples, 0.0F);
+  const Vec3 defocusDiskU(1.0F, 0.0F, 0.0F);
+  const Vec3 defocusDiskV(0.0F, 1.0F, 0.0F);
+  const float defocusAngle = 0.6F;
+
+  BENCHMARK("Single Ray Generation (curand)") {
+    benchmarkGetRay<<<1, 1>>>(d_results, origin, pixel00, deltaU, deltaV,
+                              defocusDiskU, defocusDiskV, defocusAngle,
+                              num_samples);
+    CUDA_ERROR_CHECK(cudaDeviceSynchronize());
+  };
+
+  BENCHMARK("2 Rays Generation (Philox)") {
+    benchmarkGet2Rays<<<1, 1>>>(d_results, origin, pixel00, deltaU, deltaV,
+                                defocusDiskU, defocusDiskV, defocusAngle,
+                                num_samples);
+    CUDA_ERROR_CHECK(cudaDeviceSynchronize());
+  };
+
+  BENCHMARK("4 Rays Generation (Philox)") {
+    benchmarkGet4Rays<<<1, 1>>>(d_results, origin, pixel00, deltaU, deltaV,
+                                defocusDiskU, defocusDiskV, defocusAngle,
+                                num_samples);
+    CUDA_ERROR_CHECK(cudaDeviceSynchronize());
+  };
+
+  CUDA_ERROR_CHECK(cudaFree(d_results));
+}
 
 // Test case for ray constructors
 TEST_CASE("ray constructors", "[ray]") {
@@ -43,4 +139,4 @@ TEST_CASE("ray::at", "[ray]") {
   REQUIRE(r.at(t) == expected_position);
 }
 
-// NOLINTEND(cppcoreguidelines-avoid-do-while,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+// NOLINTEND(cppcoreguidelines-pro-bounds-pointer-arithmetic,cppcoreguidelines-avoid-do-while,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
